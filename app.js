758978ae2f44789c38975a6326b87b30d3da0195
Rafael Rebolleda <hello@rafaelrebolleda.com>
2024-01-29 22:47:58 +0100

    frameCount + noLoop

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 
 function setup() {
   createCanvas(500, 500);
-  background("#000");
+  background("#FFF");
+  noFill();
+  stroke(0);
+  strokeWeight(4)
+  rect(50, 50, 400, 400)
 }
 
 function draw() {
-  fill(0);
-  stroke(255, 100); // with alpha
-  strokeWeight(2);
-
-  if (mouseIsPressed == true) {
-    myPattern(mouseX, mouseY, 20, mouseY/10);
+  if ( frameCount >= 100) {
+    noLoop();
+  } else {
+    circle(random(100, 400), random(100, 400), 4);
   }
   
 }
@@ -19,15 +21,5 @@ function keyPressed () {
   console.log("Key pressed");
   //save("export.png");
   background(0);
-
-}
-
-function myPattern(x, y, d, amp) {
-  beginShape();
-    vertex(x-d/2 + random(-amp, amp), y-d/2 + random(-amp, amp)); // 0
-    vertex(x+d/2 + random(-amp, amp), y-d/2 + random(-amp, amp)); // 1
-    vertex(x+d/2 + random(-amp, amp), y+d/2 + random(-amp, amp)); // 2
-    vertex(x-d/2 + random(-amp, amp), y+d/2 + random(-amp, amp)); // 3
-  endShape(CLOSE);
 
 }
