042a37103f37acc22d0264ae6e8b739661dd2a0e
Rafael Rebolleda <hello@rafaelrebolleda.com>
2024-01-29 21:59:48 +0100

    Drawing circles with mouse

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 
 function setup() {
   createCanvas(500, 500);
+  background("#000");
 }
 
 function draw() {
-  background(255);
-  fill("#000")
-  noStroke();
+  fill(0);
+  stroke(255);
+  strokeWeight(2);
 
-  if (mouseX < width/3) {
-    rectMode(CENTER);
-    square(width/2, height/2, 400);
-  } else if (mouseX < 2*width/3) {
-    circle(width/2, height/2, 400);
-  } else {
-    triangle(250, 50, 450, 450, 50, 450);
+  if (mouseIsPressed == true) {
+    circle(mouseX, mouseY, 50);
   }
+  
 }
 
 function keyPressed () {
   console.log("Key pressed");
+  save("export.png");
+  background(0);
+
 }
