12c1f856b8060c9eaefbf8dea092a7708094331d
Rafael Rebolleda <hello@rafaelrebolleda.com>
2024-02-06 12:42:04 +0100

    Loops and patterns, Pt 1

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
+let nb = 10;
+let dim = 0;
+let margin = 20;
 
 function setup() {
   createCanvas(500, 500);
   background("#000");
+
+  dim = (width - 2*margin)/nb;
 }
 
 function draw() {
-  noStroke();
-  fill("#FFF");
+  // noStroke();
+  // fill("#FFF");
+  stroke("#FFF");
+  noFill();
+  rectMode(CENTER);
 
-  for (let i = 0; i < 5; i++) {
-    circle(50 + i*100, 250, 100);  
+  for (let j = 0; j < nb; j++) {
+    for (let i = 0; i < nb; i++) {
+      circle(margin + dim/2 + i*dim, margin + dim/2+j*dim, dim);  
+      // rect(dim/2 + i*dim, dim/2+j*dim, 0.8*dim, 0.4*dim);  
+    }
   }
-
 }
 
 function keyPressed () {
