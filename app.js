30153e4d5426dde4cb95dd4051f874e58ef327dd
Rafael Rebolleda <hello@rafaelrebolleda.com>
2024-02-06 12:58:57 +0100

    Loops and param'd animation

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let dim = 0;
 let margin = 20;
 
 let f = 0.5; // [0..1]
-let freq = 0.7;
+let freq = 1;
 
 let x, y;
 
@@ -20,13 +20,13 @@ function draw() {
   noFill();
   rectMode(CENTER);
 
-  f = sin(frameCount * freq);
-
   for (let j = 0; j < nb; j++) {
     for (let i = 0; i < nb; i++) {
 
       x = margin + dim/2 + i*dim;
       y = margin + dim/2+j*dim;
+
+      f = sin(frameCount * freq + dist(width/2, height/2, x, y));
 
       circle(x, y, f*dim);  
       // rect(dim/2 + i*dim, dim/2+j*dim, 0.8*dim, 0.4*dim);  
