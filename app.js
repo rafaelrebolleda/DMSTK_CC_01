bb7fe34ce2aefeb05dc5fec71786392a86e8e841
Rafael Rebolleda <hello@rafaelrebolleda.com>
2024-02-06 12:56:12 +0100

    Loops and Patterns: animation

--- app.js
+++ app.js
@@ -2,23 +2,33 @@ let nb = 10;
 let dim = 0;
 let margin = 20;
 
+let f = 0.5; // [0..1]
+let freq = 0.7;
+
+let x, y;
+
 function setup() {
   createCanvas(500, 500);
-  background("#000");
+  angleMode(DEGREES);
 
   dim = (width - 2*margin)/nb;
 }
 
 function draw() {
-  // noStroke();
-  // fill("#FFF");
+  background("#000");
   stroke("#FFF");
   noFill();
   rectMode(CENTER);
 
+  f = sin(frameCount * freq);
+
   for (let j = 0; j < nb; j++) {
     for (let i = 0; i < nb; i++) {
-      circle(margin + dim/2 + i*dim, margin + dim/2+j*dim, dim);  
+
+      x = margin + dim/2 + i*dim;
+      y = margin + dim/2+j*dim;
+
+      circle(x, y, f*dim);  
       // rect(dim/2 + i*dim, dim/2+j*dim, 0.8*dim, 0.4*dim);  
     }
   }
