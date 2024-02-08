18fe4fd288fd3d880bea600ec1d335d5c30bf659
Rafael Rebolleda <hello@rafaelrebolleda.com>
2024-02-08 12:18:22 +0100

    Basic translation and rotation

--- app.js
+++ app.js
@@ -1,55 +1,32 @@
-let nb = 8;
-let dim = 0;
-let margin = 20;
-let x, y;
+let nb = 10;
 
 
 function setup() {
-  createCanvas(500, 500);
-  dim = (width - 2*margin)/nb;
-
-  noLoop();
+  createCanvas(500, 500); 
+  rectMode(CENTER);
+  angleMode(DEGREES);
 }
 
 function draw() {
-  background("#FFF");
+  background("#000 ");
 
-  for (let j = 0; j < nb; j++) {
-    for (let i = 0; i < nb; i++) {
+  // Change origin
+  translate(width/2, height/2);
+  
 
-      x = margin + i*dim;
-      y = margin + j*dim;
- 
-      noFill();
-      stroke("#DDD");
+  stroke("#FFF");
+  noFill();
 
-      // GRID
-      // rect(x, y, dim, dim);
-
-      // Create different patterns randomly
-      let rnd = int(random(0, 4));
-      noStroke();
-      fill("#000");
-      
-
-      if ( rnd == 0 ) {
-        triangle(x, y, x+dim, y+dim, x, y+dim)
-      } else if ( rnd == 1) {
-        triangle(x, y, x+dim, y+dim, x+dim, y)
-      } else if ( rnd == 2) {
-        triangle(x+dim, y, x+dim, y+dim, x, y+dim)
-      } else if ( rnd == 3) {
-        triangle(x, y, x+dim, y, x, y+dim)
-      }
-    
-
-    }
+  for (let i = 0; i < nb; i++) {
+    square(0, 0, 350 );
+    rotate(10); 
   }
+
+  
 }
  
 function keyPressed () {
   console.log("Key pressed");
   //save("export.png");
-  redraw();
 
 }
