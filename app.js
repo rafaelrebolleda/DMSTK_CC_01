4cc623a8848ecc7cf70bafbec33481b47515a87f
Rafael Rebolleda <hello@rafaelrebolleda.com>
2024-02-08 12:47:32 +0100

    Spirograph with param UI

--- app.js
+++ app.js
@@ -5,18 +5,22 @@ let dim = 350;
 let f = 1;
 let fmin = 0.2;
 
-let sliderNb;
+let sliderNb, sliderRot, sliderFmin;
 
 function setup() {
   createCanvas(500, 500); 
   rectMode(CENTER);
   angleMode(DEGREES);
 
-  sliderNb = createSlider(2, 50, nb, 1);
+  sliderNb   = createSlider(2, 50, nb, 1);
+  sliderRot  = createSlider(2, 90, rot);
+  sliderFmin = createSlider(0.0, 1.0, fmin, 0.1);
 }
 
 function draw() {
-  nb = sliderNb.value();
+  nb  = sliderNb.value();
+  rot = sliderRot.value();
+  fmin = sliderFmin.value();
 
   background("#000 ");
 
