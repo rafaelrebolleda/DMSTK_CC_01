dbde011310bdd7673bde3452f600e5b4b1992cad
Rafael Rebolleda <hello@rafaelrebolleda.com>
2024-02-08 12:40:15 +0100

    Add slider UI to control prams

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
-let nb = 10;
+let nb = 30;
+let rot = 1;
+let dim = 350;
 
+let f = 1;
+let fmin = 0.2;
+
+let sliderNb;
 
 function setup() {
   createCanvas(500, 500); 
   rectMode(CENTER);
   angleMode(DEGREES);
+
+  sliderNb = createSlider(2, 50, nb, 1);
 }
 
 function draw() {
+  nb = sliderNb.value();
+
   background("#000 ");
 
   // Change origin
   translate(width/2, height/2);
   
-
   stroke("#FFF");
   noFill();
 
   for (let i = 0; i < nb; i++) {
-    square(0, 0, 350 );
-    rotate(10); 
-  }
 
-  
+    f = map(i, 0, nb - 1, 1, fmin);
+
+    square(0, 0, f * dim );
+    rotate(rot); 
+  }
 }
  
 function keyPressed () {
