933e9c20a6fbfc5d776fe9c33697d2542a44f71e
Rafael Rebolleda <hello@rafaelrebolleda.com>
2024-02-12 12:07:53 +0100

    Simple particle system

--- app.js
+++ app.js
@@ -1,44 +1,55 @@
-let nb = 30;
-let rot = 1;
-let dim = 350;
 
-let f = 1;
-let fmin = 0.2;
+let p = Array(100);
 
-let sliderNb, sliderRot, sliderFmin;
 
 function setup() {
   createCanvas(500, 500); 
-  rectMode(CENTER);
-  angleMode(DEGREES);
 
-  sliderNb   = createSlider(2, 50, nb, 1);
-  sliderRot  = createSlider(2, 90, rot);
-  sliderFmin = createSlider(0.0, 1.0, fmin, 0.1);
+  for (let i = 0; i < p.length; i++) {
+    p[i] = new Particle(250, 250);    
+  }
+
 }
 
 function draw() {
-  nb  = sliderNb.value();
-  rot = sliderRot.value();
-  fmin = sliderFmin.value();
 
-  background("#000 ");
+  background("#000 "); 
+  noStroke();
+  fill("#FFF");
 
-  // Change origin
-  translate(width/2, height/2);
-  
-  stroke("#FFF");
-  noFill();
-
-  for (let i = 0; i < nb; i++) {
-
-    f = map(i, 0, nb - 1, 1, fmin);
-
-    square(0, 0, f * dim );
-    rotate(rot); 
+  for (let i = 0; i < p.length; i++) {
+    p[i].draw();
   }
+
 }
- 
+
+class Particle {
+
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.vx = random(-2, 2);
+    this.vy = random(-2, 2);
+  }
+
+  draw() {
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+
+    if (this.x < 0 || this.x > width || this.y < 0 || this.y > height) {
+      this.x = random(0, width);
+      this.y = random(0, height);
+      this.vx = random(-2, 2);
+      this.vy = random(-2, 2);
+      }
+
+    circle(this.x, this.y, 5);
+  }
+
+}
+
+
+
 function keyPressed () {
   console.log("Key pressed");
   //save("export.png");
